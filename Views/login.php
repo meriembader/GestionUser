3d73dbe6076e72887b4d55fe7d48fb648508ffdd
Meriem BADER <meriembader8@gmail.com>
2021-01-01 20:38:20 +0100

    done

--- Views/login.php
+++ Views/login.php
@@ -155,7 +155,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
+        </form>   
     </div>    
 
     <!-- Jquery JS-->
